9c09123c56019f371c24ff5aa439af3b37419e15
unknown <bieldanil240@gamil.com>
2025-01-15 18:00:20 +0300

    hw-3

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+//1
+const result = 5 + 5 + "5";
+console.log(result);
+
+//2
+const myEmail = "daniilbielevtsev@gmail.com";
+const contolEmail = Email.includes("@");
+console.log(`Email містить символ'@'? - ${contolEmail}`);
+console.log(myEmail.length);
+
+//3
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+const fullName = `${word1} ${word2} ${word3} Viktor`;
+console.log(fullName);
+
+//4
+const userName = "Даніїл";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
